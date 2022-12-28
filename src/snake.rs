96cc1cdb209557ed6ba229214f7d78c9b92b9fd6
Adi De Masi <adi.demasi@homegate.ch>
2022-12-28 17:57:24 +0100

    Snake does not grow anymore.

--- src/snake.rs
+++ src/snake.rs
@@ -9,33 +9,44 @@ impl Snake {
             coords: vec![(start_pos)],
         };
     }
-    pub fn _move(&mut self, key: Key) {
+    pub fn move_to(&self, key: &Key) -> Snake {
+        let mut snake_coords = self.coords.clone();
         let next = self.next_coord(key);
         match next {
-            Some(next) => self.coords.push(next),
+            Some(next) => {
+                snake_coords.push(next);
+                snake_coords.remove(0);
+            }
             None => (),
         }
+        return Snake {
+            coords: snake_coords,
+        };
     }
-    pub fn next_coord(&mut self, direction: Key) -> Option<(i32, i32)> {
-        if let Some(last) = self.coords.pop() {
-            self.coords.push(last);
-            match direction {
-                Key::ArrowLeft => {
-                    return Some((last.0, last.1 - 1));
+    pub fn next_coord(&self, direction: &Key) -> Option<(i32, i32)> {
+        let last = self.coords.get(self.coords.len() - 1);
+        match last {
+            Some(last) => {
+                match direction {
+                    Key::ArrowLeft => {
+                        return Some((last.0, last.1 - 1));
+                    }
+                    Key::ArrowRight => {
+                        return Some((last.0, last.1 + 1));
+                    }
+                    Key::ArrowUp => {
+                        return Some((last.0 - 1, last.1));
+                    }
+                    Key::ArrowDown => {
+                        return Some((last.0 + 1, last.1));
+                    }
+                    _ => return None,
                 }
-                Key::ArrowRight => {
-                    return Some((last.0, last.1 + 1));
-                }
-                Key::ArrowUp => {
-                    return Some((last.0 - 1, last.1));
-                }
-                Key::ArrowDown => {
-                    return Some((last.0 + 1, last.1));
-                }
-                _ => return None,
+            }
+            None => {
+                return None
             }
         }
-        return None;
     }
 
     pub fn is_occupied(&self, pos: (i32, i32)) -> bool {
@@ -58,26 +69,26 @@ mod tests {
     #[test]
     fn it_moves_left() {
         let mut snake = crate::snake::Snake::new((2, 2));
-        snake._move(Key::ArrowLeft);
-        assert_eq!(vec!((2, 2), (2, 1)), snake.coords);
+        snake = snake.move_to(&Key::ArrowLeft);
+        assert_eq!(vec!((2, 1)), snake.coords);
     }
     #[test]
     fn it_moves_right() {
         let mut snake = crate::snake::Snake::new((2, 2));
-        snake._move(Key::ArrowRight);
-        assert_eq!(vec!((2, 2), (2, 3)), snake.coords);
+        snake = snake.move_to(&Key::ArrowRight);
+        assert_eq!(vec!((2, 3)), snake.coords);
     }
     #[test]
     fn it_moves_up() {
         let mut snake = crate::snake::Snake::new((2, 2));
-        snake._move(Key::ArrowUp);
-        assert_eq!(vec!((2, 2), (1, 2)), snake.coords);
+        snake = snake.move_to(&Key::ArrowUp);
+        assert_eq!(vec!((1, 2)), snake.coords);
     }
     #[test]
     fn it_moves_down() {
         let mut snake = crate::snake::Snake::new((2, 2));
-        snake._move(Key::ArrowDown);
-        assert_eq!(vec!((2, 2), (3, 2)), snake.coords);
+        snake = snake.move_to(&Key::ArrowDown);
+        assert_eq!(vec!((3, 2)), snake.coords);
     }
     #[test]
     fn is_occupied() {
